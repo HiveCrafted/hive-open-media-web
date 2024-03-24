6570266a5583c77852666ddd57088a3d5baeb76a
makijurowski <18458620+makijurowski@users.noreply.github.com>
2024-03-23 22:31:12 -0500

    add theming

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { ConfigProvider, Card, Button, theme } from 'antd';
+import GoogleSignIn from './components/GoogleSignIn';
+import SelectSource from './components/SelectSource';
+// import CreateFolder from './components/CreateFolder';
+import './App.css';
+
+const logo = require('./assets/images/logo-1.png');
+
+function App() {
+  const { defaultAlgorithm, darkAlgorithm } = theme;
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleClick = () => {
+    setIsDarkMode((previousValue) => !previousValue);
+  };
+
+  return (
+    <div className="App">
+      <ConfigProvider
+        theme={{
+          algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+        }}
+      >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Card title="Hive Open Media" style={{ width: 'max-content' }}>
+          <Button onClick={handleClick}>Change Theme to {isDarkMode ? 'Light' : 'Dark'}</Button>
+          <GoogleSignIn />
+          <SelectSource />
+          {/* <CreateFolder /> */}
+        </Card>
+      </ConfigProvider>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <h1>something</h1>
   </React.StrictMode>
 );
 
